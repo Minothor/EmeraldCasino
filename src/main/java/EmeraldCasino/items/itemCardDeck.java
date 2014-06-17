f56f0a6fdc94e4cd5b4d65a6a18803df6995bdae
NJB <njboyd@gmail.com>
2014-06-17 16:09:13 +0200

    still not working

--- src/main/java/EmeraldCasino/items/itemCardDeck.java
+++ src/main/java/EmeraldCasino/items/itemCardDeck.java
@@ -2,6 +2,7 @@ package EmeraldCasino.items;
 
 import EmeraldCasino.EmeraldCasino;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
@@ -33,21 +34,31 @@ public class itemCardDeck extends Item{
 	
 	@Override
 	public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ){
-		/*if(player.isSneaking()){
+		if(player.isSneaking())
+		{
 		incrementDamage(stack);
 		System.out.println("Dmg Val: "+stack.getItemDamage());
 		return true;
-		}*/
-		if((world.getBlock(x, y, z)!=Blocks.air)&&(world.getBlock(x, y+1, z)==Blocks.air)){	
-		world.setBlock(x, y+1, z, GameRegistry.findBlock(EmeraldCasino.MODID, "cardDeck"));
-		//world.setBlockMetadataWithNotify(x, y+1, z, stack.getItemDamage(), 2);
-		return true;
+		}
+		else if((world.getBlock(x, y, z)!=Blocks.air)&&(world.getBlock(x, y+1, z)==Blocks.air))
+			{
+			Block temp =  GameRegistry.findBlock(EmeraldCasino.MODID, "cardDeck");
+			if (temp!= null){
+				world.setBlock(x, y+1, z,temp);
+				//world.setBlockMetadataWithNotify(x, y+1, z, stack.getItemDamage(), 2);
+				return true;
+			}else{
+			System.out.println("Null Returned!");
+		
+		}
+			
 		}
 		return false;
 	}
 
 	private void incrementDamage(ItemStack stack) {
-		stack.setItemDamage(1+stack.getItemDamage());
+		int temp = stack.getItemDamage();
+		stack.setItemDamage(1+temp);
 		if(stack.getItemDamage()>stack.getMaxDamage()){
 			stack.setItemDamage(stack.getItemDamage()-stack.getMaxDamage());
 		}
