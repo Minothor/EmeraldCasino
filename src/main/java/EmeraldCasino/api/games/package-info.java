7c15659de984229a27ace948233bee83650eccce
Nick Boyd <njboyd@gmail.com>
2014-06-27 15:14:18 +0200

    API housekeeping

---
+++ src/main/java/EmeraldCasino/api/games/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Emerald Casino API
+ * 
+ */
+@API(apiVersion = "1.0", owner = "Minothor(for now)", provides = "ECAPI|games")
+package emeraldCasino.api.games;
+
+import cpw.mods.fml.common.API;
