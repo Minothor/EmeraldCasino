22e31472d28a7b204d1f3ee0ee2ac16247927197
Nick Boyd <njboyd@gmail.com>
2014-09-15 16:33:13 +0200

    experimental handler for carddeck crafting added

---
+++ src/main/java/EmeraldCasino/crafting/CraftingHandler.java
@@ -0,0 +1,11 @@
+package emeraldCasino.crafting;
+
+import net.minecraft.item.crafting.CraftingManager;
+
+public class CraftingHandler {
+	public CraftingManager cM;
+	public CraftingHandler() {
+		cM = CraftingManager.getInstance();
+	}
+
+}
